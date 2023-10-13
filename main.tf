f25a01bdc05308c67c5d9f921bbdee12bf1de61e
Toty8 <80042351+Toty8@users.noreply.github.com>
2023-10-13 17:31:07 +0300

    Fix the db size

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ resource "azurerm_mssql_database" "db" {
   server_id      = azurerm_mssql_server.dbs.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
-  max_size_gb    = 4
+  max_size_gb    = 1
   sku_name       = "S0"
   zone_redundant = false
 }
